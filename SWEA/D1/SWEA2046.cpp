02b99938200210a18c798b19ccd75c3f600c41c4
kch1285 <kch1285@naver.com>
2021-03-29 19:13:15 +0900

    SWEA2046

---
+++ SWEA/D1/SWEA2046.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	int input;
+    cin >> input;
+    for(int i=0;i<input;i++){
+        cout << "#";
+    }
+	return 0;
+}
