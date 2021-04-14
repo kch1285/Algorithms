1027e2c7cba76b7aee958128a7f1c54a9acef9d7
kch1285 <kch1285@naver.com>
2021-04-14 15:54:38 +0900

    BaekJoon7662

--- BaekJoon/7662.cpp
+++ BaekJoon/7662.cpp
@@ -1,27 +1,45 @@
 #include <iostream>
-#include <queue>
+#include <map>
 
 using namespace std;
 
-priority_queue<int> pq;
-
 int main(){
-    cin.tie(NULL);
-    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
 
-    int t, k, n, ans;
-    char dOri;
+    int t, k, num;
+    char oper;
+
     cin >> t;
     for(int i=0;i<t;i++){
         cin >> k;
+        map<int, int> q;
+
         for(int j=0;j<k;j++){
-            cin >> dOri >> n;
-            if(dOri == 'I') pq.push(n);
-            else if(dOri == 'D'){
-                if(n == 1)  pq.pop();
+            cin >> oper >> num;
+
+            if(oper == 'I'){
+                if(q.find(num) != q.end())  q[num]++;
+                else    q[num] = 0;
+            }
+            else if(oper == 'D'){
+                if(q.empty())   continue;
+                if(num == 1){
+                    auto it = prev(q.end());
+                    if(it->second > 0)  it->second--;
+                    else    q.erase(it);
+                }
+                else if(num == -1){
+                    auto it = q.begin();
+                    if(it->second > 0)  it->second--;
+                    else    q.erase(it);
+                }
             }
         }
-        if(pq.empty)    cout << "EMPTY" << '\n';
-        else    cout << pq.top() << '\n';
+
+        if(q.empty())   cout << "EMPTY" << '\n';
+        else    cout << prev(q.end())->first << ' ' << q.begin()->first << '\n';
     }
+    return 0;
 }
